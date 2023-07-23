0e646f1df19047e04d6b013742ad2a07fed2a5ae
Vincent Girard <vinccool96@gmail.com>
2023-07-23 17:04:30 -0400

    Update

---
+++ core/commonMain/src/org/cirjson/serialization/internal/CircularSerializationConstructorMarker.kt
@@ -0,0 +1,5 @@
+package org.cirjson.serialization.internal
+
+@Suppress("UNUSED")
+@Deprecated("Inserted into generated code and should not be used directly", level = DeprecationLevel.HIDDEN)
+public class CircularSerializationConstructorMarker private constructor()
